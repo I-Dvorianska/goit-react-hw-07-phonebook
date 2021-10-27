64b580682a450c13453b8deb9c1664e09ca1169a
Ivanka <idvoryanska@gmail.com>
2021-10-27 17:24:33 +0200

    memo

--- src/redux/Selectors/contactListSelectors.js
+++ src/redux/Selectors/contactListSelectors.js
@@ -1,16 +1,19 @@
+import { createSelector } from "@reduxjs/toolkit";
+
 export const getFilter = (state) => state.contacts.filter;
 export const getContacts = (state) => state.contacts.items;
 
-export const visibleContacts = (state) => {
-  const filter = getFilter(state);
-  const contacts = getContacts(state);
-  if (filter !== "") {
-    if (contacts !== []) {
-      return contacts.filter((contact) =>
-        contact.name.toLowerCase().includes(filter)
-      );
+export const visibleContacts = createSelector(
+  [getFilter, getContacts],
+  (filter, contacts) => {
+    if (filter !== "") {
+      if (contacts !== []) {
+        return contacts.filter((contact) =>
+          contact.name.toLowerCase().includes(filter)
+        );
+      }
+    } else {
+      return contacts;
     }
-  } else {
-    return contacts;
   }
-};
+);
